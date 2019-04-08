633330db0ab567499c02ee86f20416881cda0418
Ravi Sankar <sankarr@stanford.edu>
2019-04-08 15:17:21 -0700

    Basic integration with the mobilize events API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+      nextUrl: undefined,
+      prevUrl: undefined,
+      totalCount: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchEvents();
+  }
+
+  fetchEvents = (params = {}) => {
+    const nowInSeconds = Math.floor(new Date().getTime() / 1000);
+    axios
+      .get('https://api.mobilize.us/v1/events', {
+        ...params,
+        timeslot_start: `gte_${nowInSeconds}`, // always filter to events that still active
+      })
+      .then(({ data }) => {
+        this.setState({
+          events: data.data,
+          nextUrl: data.next,
+          prevUrl: data.previous,
+          totalCount: data.count,
+        });
+      })
+      .catch(({ error }) => {
+        // todo
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.events.map(event => {
+            return <p>{event.title}</p>;
+          })}
         </header>
       </div>
     );
