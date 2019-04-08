30f2027e1e515d8053f6edf310cf53b7c13bb8f8
Ravi Sankar <sankarr@stanford.edu>
2019-04-08 16:33:35 -0700

    More full featured event listing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import moment from 'moment-timezone';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Avatar,
+  Paper,
+  ListItemAvatar,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+const Header = () => (
+  <AppBar position="absolute" color="default">
+    <Toolbar>
+      <Typography variant="h6" color="inherit">
+        MobilizeAmerica: Find a way to change politics
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      avatarsWithErrors: {},
       events: [],
       nextUrl: undefined,
       prevUrl: undefined,
@@ -23,8 +57,10 @@ class App extends Component {
     const nowInSeconds = Math.floor(new Date().getTime() / 1000);
     axios
       .get('https://api.mobilize.us/v1/events', {
-        ...params,
-        timeslot_start: `gte_${nowInSeconds}`, // always filter to events that still active
+        params: {
+          ...params,
+          timeslot_start: `gte_${nowInSeconds}`, // always filter to events that still active
+        },
       })
       .then(({ data }) => {
         this.setState({
@@ -39,17 +75,86 @@ class App extends Component {
       });
   };
 
+  renderDate = event => {
+    if (!event.timeslots && event.timeslots.length === 0) {
+      return null;
+    }
+
+    const startTime = moment.unix(event.timeslots[0].start_date);
+    const zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const zoneString = moment.tz(zone).format('z');
+    const formatted =
+      startTime.format('ddd, MMM Do YYYY hA z') + ' ' + zoneString;
+    if (event.timeslots.length === 1) {
+      return formatted;
+    } else if (event.timeslots.length >= 1) {
+      return `${event.timeslots.length} events starting ${formatted}`;
+    }
+  };
+
+  renderEventList = () => {
+    const { classes } = this.props;
+    return (
+      <List className={classes.list}>
+        {this.state.events.map(event => (
+          <ListItem alignItems="flex-start" button={true} divider={true}>
+            {event.featured_image_url &&
+            !this.state.avatarsWithErrors[event.id] ? (
+              <ListItemAvatar>
+                <Avatar
+                  alt="Profile Picture"
+                  src={event.featured_image_url}
+                  imgProps={{
+                    onError: e => {
+                      this.setState({
+                        avatarsWithErrors: {
+                          ...this.state.avatarsWithErrors,
+                          [event.id]: true,
+                        },
+                      });
+                    },
+                  }}
+                />
+              </ListItemAvatar>
+            ) : (
+              <div className="List__empty-avatar" />
+            )}
+            <ListItemText
+              primary={event.title}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    {this.renderDate(event)}
+                  </Typography>
+                  <br />
+                  {event.description && event.description.length > 200
+                    ? event.description.substring(0, 200) + '...'
+                    : event.description}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          {this.state.events.map(event => {
-            return <p>{event.title}</p>;
-          })}
-        </header>
+        <Header />
+        <div className="App__left">
+          <Paper className={classes.root}>{this.renderEventList()}</Paper>
+        </div>
+        <div className="App__right" />
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
